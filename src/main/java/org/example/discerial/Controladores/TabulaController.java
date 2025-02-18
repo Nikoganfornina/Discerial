5a56da83162d830aa7f8422405e07aa8898d94e8
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2025-02-18 20:57:34 +0100

    cOMMIT

--- src/main/java/org/example/discerial/Controladores/TabulaController.java
+++ src/main/java/org/example/discerial/Controladores/TabulaController.java
@@ -1,4 +1,28 @@
 package org.example.discerial.Controladores;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class TabulaController {
+
+    @FXML
+    private Pane contenedorFXML;
+
+    @FXML
+    private void cargarFXMLPequeno() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/discerial/Panels/HomoPanel_view.fxml"));
+            Parent nuevoContenido = loader.load();
+
+            // Limpiar el contenedor antes de añadir el nuevo contenido
+            contenedorFXML.getChildren().clear();
+            contenedorFXML.getChildren().add(nuevoContenido);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
